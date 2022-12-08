4e4c8c67b318f26004aa2f41c1d10cd0f689e9f8
Muhammad Amaan <bitf19m016@pucit.edu.pk>
2022-12-08 23:49:28 +0500

    Added essetial UI elements

--- app/src/main/java/com/example/quranversefinder/MainActivity.java
+++ app/src/main/java/com/example/quranversefinder/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.quranversefinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView Verse;
+    public TextView SuraNum;
+    public TextView VerseNum;
+    public Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Verse = findViewById(R.id.Verse);
+        btnSearch = findViewById(R.id.btnSearch);
+        SuraNum = findViewById(R.id.SuraNum);
+        VerseNum = findViewById(R.id.VerseNum);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Verse.setText("I am working");
+            }
+        });
+
     }
 }
